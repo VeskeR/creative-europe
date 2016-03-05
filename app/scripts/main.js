f89c31edfc8fd13e60270d86e850939284dced78
Andrew Bulat <andrii.bulat@gmail.com>
2016-03-05 21:53:44 +0200

    Added all functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,13 @@ $(document).ready(() => {
           console.error('No partial name given.');
           return '';
       }
-      var partial = app.templates[partialName];
+      var parts = partialName.split('.');
+      var partial = app.templates;
+      for (var i = 0; i < parts.length; i++) {
+        if (partial === null || partial === undefined)
+          break;
+        partial = partial[parts[i]];
+      }
       if (!partial) {
           console.error('Couldnt find the compiled partial: ' + partialName);
           return '';
@@ -15,41 +21,75 @@ $(document).ready(() => {
   });
 
   var content = $('.content');
+  var currentPageName;
   var currentPageNumber;
+  var currentPageCount;
 
   var page = {
     home: 'home',
-    about: 'about'
-  };
-  
-  var templates = {
-    home: [
-      'home-1',
-      'home-2',
-      'home-3'
-    ],
-    about: [
-      'about-1',
-      'about-2'
-    ]
+    about: 'about',
+    culture: 'culture'
   };
 
   function renderContent(pageName, pageNumber) {
-    var pageTemplate = templates[pageName];
-    var pageCount = pageTemplate.length;
-    if (pageNumber >= 0 && pageNumber < pageCount) {
-      var template = pageTemplate[pageNumber - 1];
+    var pageTemplates = app.templates[pageName];
+    if (pageTemplates.hasOwnProperty(pageNumber)) {
+      currentPageName = pageName;
+      currentPageNumber = pageNumber;
+      currentPageCount = countObjectProperties(pageTemplates);
+
+      var templateName = pageName + '.' + pageNumber;
+
       content.html(app.templates.content({
-        template: template,
-        currentPage: pageNumber,
-        pageCount: pageCount
+        template: templateName,
+        currentPage: currentPageNumber,
+        pageCount: currentPageCount
       }));
+
+      bindNavArrows();
+      checkNavArrows();
+      checkNavigationMenu();
     } else {
       console.log('Wrong page number ' + pageNumber + ' for page ' + pageName);
     }
   };
 
+  function countObjectProperties(o) {
+    var count = 0;
+    for (var p in o)
+      if (o.hasOwnProperty(p)) count++;
+    return count;
+  }
 
+  function bindNavArrows() {
+    $('.main .content__navigation .icon--left').on('click', function() {
+      renderContent(currentPageName, currentPageNumber - 1);
+    });
+    $('.main .content__navigation .icon--right').on('click', function() {
+      renderContent(currentPageName, currentPageNumber + 1);
+    });
+  }
+
+  function checkNavArrows() {
+    $('.main .content__navigation .icon').removeClass('disabled');
+
+    if (currentPageNumber === 1) {
+      $('.main .content__navigation .icon--left').addClass('disabled');
+    }
+    if (currentPageNumber === currentPageCount) {
+      $('.main .content__navigation .icon--right').addClass('disabled');
+    }
+  }
+
+  function checkNavigationMenu() {
+    $('.nav .list .list--item').removeClass('selected');
+    $('.nav .list .list--item[data-page="' + currentPageName + '"]').addClass('selected');
+  }
+
+
+  $('.nav .list .list--item').on('click', function(e) {
+    renderContent($(e.target).data('page'), 1);
+  });
 
   renderContent(page.home, 1);
 });
